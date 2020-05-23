bf67c909dbe10e22556caf5661d5fbc96ea3d253
Lucas Schell <lucas.schell@acad.pucrs.br>
2020-05-23 20:20:59 -0300

    full game

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,12 @@
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,12 +20,15 @@ public class Game {
         this.players = players;
     }
 
-    public String getQuestion(int index) throws FileNotFoundException {
-        File f = new File("perguntas.txt");
-        Scanner sc = new Scanner(f);
+    public String[] getQuestion(String dif, String index) throws IOException, JSONException {
+        JSONObject json = new JSONObject("respostas.json");
+        String[] question = new String[2];
 
-        qustionCorrect = "";
-        return "";
+        question[0] = json.getJSONObject(dif).getJSONObject(index).getString("pergunta");
+        question[1] = json.getJSONObject(dif).getJSONObject(index).getString("respostas");
+        qustionCorrect = json.getJSONObject(dif).getJSONObject(index).getString("gabarito");
+
+        return question;
     }
 
     public void addPoints(ArrayList<String> answers, int dif) {
@@ -26,7 +36,7 @@ public class Game {
 
         for (String str : answers) {
             String[] ans = str.split(" ");
-            if (qustionCorrect.equals(ans[0])) {
+            if (qustionCorrect.equalsIgnoreCase(ans[0])) {
                 Player p = getPlayer(ans[1]);
                 if (p != null) {
                     getPlayer(ans[1]).addPoints(points);
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,49 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Carlos gay!");
 
 
+    }
+
+    public void start() throws IOException {
+        DatagramSocket serverSocket = new DatagramSocket(9876);
+
+        byte[] receiveData = new byte[1024];
+        while (true) {
+            // declara o pacote a ser recebido
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+            // recebe o pacote do cliente
+            serverSocket.receive(receivePacket);
+        }
+    }
+
+    public ArrayList<String[]> questionOrder(int dif) {
+        ArrayList<String[]> list = new ArrayList<>();
+        Random r = new Random();
+        int cont = dif == 0 ? 4 : 3;
+        for (int i = 0; i < cont; i++) {
+            list.add(new String[]{dif + "", i + ""});
+        }
+        if (dif == 0) {
+            list.add(new String[]{"1", r.nextInt(3) + ""});
+        } else {
+            if (dif == 1) {
+                list.add(new String[]{"0", r.nextInt(4) + ""});
+                list.add(new String[]{"2", r.nextInt(3) + ""});
+            } else {
+                list.add(new String[]{"1", r.nextInt(3) + ""});
+                list.add(new String[]{"1", r.nextInt(3) + ""});
+            }
+        }
+        return list;
     }
 }
