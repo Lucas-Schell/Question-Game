e40ef4c4820c52475dcc114b1783b8402ccd90df
Lotusdigital <42621099+Lotusdigital398@users.noreply.github.com>
2020-05-24 19:19:10 -0300

    classe player comentada

--- src/Player.java
+++ src/Player.java
@@ -8,6 +8,8 @@ public class Player {
     private InetAddress ip;
 
     private int port;
+
+    // construtor do objeto aluno, contendo todas as informacoes importantes
     public Player(String name, InetAddress ip, int port){
         this.name = name;
         this.ip = ip;
@@ -15,22 +17,27 @@ public class Player {
         points = 0;
     }
 
+    // caso o jogador pontue, serão adicionados pontos no seu total
     public void addPoints(int pts){
         points += pts;
     }
 
+    // retorna o IP do jogador
     public InetAddress getIp() {
         return ip;
     }
 
+    // retorna a porta do jogador
     public int getPort() {
         return port;
     }
 
+    // caso seja necessario consultar os pontos atuais do jogador, esse metodo sera utilizado
     public int getPoints(){
         return points;
     }
 
+    // retornara o nome do jogador
     public String getName(){
         return name;
     }
